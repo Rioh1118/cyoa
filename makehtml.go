92082d5dd99023380a9a2320bea37309967636e6
Rioh1118 <rio2515t@gmail.com>
2023-10-25 10:10:22 +0900

    correct makehtml

--- makehtml.go
+++ makehtml.go
@@ -9,27 +9,24 @@ import (
 
 var tpl *template.Template
 
-func MakeHtml(fileName string, key string, story Story) error {
+func MakeHtml(tempPath string, filePath string, chapter Chapter) error {
 	// 指定されたファイル名が.htmlで終わっているかを確認
-	if !strings.HasSuffix(fileName, ".html") {
+	if !strings.HasSuffix(filePath, ".html") {
 		return errors.New("file name isn't an html file")
 	}
 
-	// テンプレートのパスを指定
-	templatePath := "templates/format.gohtml"
-
 	// テンプレートをパース
-	tpl = template.Must(template.ParseFiles(templatePath))
+	tpl = template.Must(template.ParseFiles(tempPath))
 
 	// 出力ファイルを作成
-	outputFile, err := os.Create(fileName)
+	outputFile, err := os.Create(filePath)
 	if err != nil {
 		return err
 	}
 	defer outputFile.Close()
 
 	// テンプレートを実行し、結果を出力ファイルに書き込む
-	err = tpl.ExecuteTemplate(outputFile, templatePath, story[key])
+	err = tpl.ExecuteTemplate(outputFile, tempPath, chapter)
 	if err != nil {
 		return err
 	}
