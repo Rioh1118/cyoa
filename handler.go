00a6ad3e62fff737d4ba12e11e3cabb118e616ec
Rioh1118 <rio2515t@gmail.com>
2023-10-25 16:53:48 +0900

    implement handler.go

--- handler.go
+++ handler.go
@@ -1,7 +1,39 @@
 package cyoa
 
+import (
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
 /*
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.
-}
+map
+index intro.html => /
+other key.html => /key
 */
+func MapHandler(story Story, dirPath string) http.HandlerFunc {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			path := r.URL.Path
+			key := strings.TrimPrefix(path, "/")
+
+			if _, ok := story[key]; !ok {
+				http.NotFound(w, r)
+				return
+			}
+			filePath := dirPath + "/" + path + ".html"
+
+			file, err := os.Open(filePath)
+			if err != nil {
+				http.Error(w, "File not found", http.StatusNotFound)
+				return
+			}
+
+			_, err = io.Copy(w, file)
+			if err != nil {
+				http.Error(w, "Failed to send response", http.StatusInternalServerError)
+				return
+			}
+		})
+}
